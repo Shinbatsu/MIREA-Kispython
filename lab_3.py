fa22707589cd89232978f7eb48ee08baa9ec3d44
Shinbatsu <nojic132@gmail.com>
2020-10-29 09:06:16 +0300

    solve lab 3

---
+++ lab_3.py
@@ -0,0 +1,6 @@
+# Solve Math row expersion wiht Sum and Product operations over math unints
+
+def main(n,b,p): # 3
+    return sum(64+55*(i**2+i)**6+51*i-75*i**2-
+            11for i in range(1,n+1))-sum(sum(95*j**3-95*
+            c-p**4for j in range(1,b+1))for c in range(1,n+1))
